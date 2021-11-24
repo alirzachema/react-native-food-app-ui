22db2340517233c933a9a3be1afa6f718c1fb034
alirzachema <38101724+alirzachema@users.noreply.github.com>
2021-11-24 17:12:37 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import 'react-native-gesture-handler'
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaView } from 'react-native';
+
+import COLORS from './src/consts/colors'
+import OnBoardScreen from './src/views/screens/OnBoardScreen';
+import DetailsScreen from './src/views/screens/DetailsScreen';
+import BottomNavigator from './src/views/navigation/BottomNavigator';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <StatusBar backgroundColor={COLORS.white} barStyle="dark-content" />
+            <Stack.Navigator screenOptions={{headerShown: false}}>
+                <Stack.Screen name="BoardScreen" component={OnBoardScreen} />
+                <Stack.Screen name="Home" component={BottomNavigator} />
+                <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
